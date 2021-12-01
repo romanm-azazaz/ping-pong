2566c25b02577967aa505423035ecc2ccda94487
Roman Makarov <57663222+romanm-azazaz@users.noreply.github.com>
2021-12-01 12:45:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import argparse
+from http.server import BaseHTTPRequestHandler, HTTPServer; import argparse
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        self.send_response(200); self.send_header("Content-type", "text/html"); self.end_headers()
         print(f"Request headers:\n{self.headers}")
         if validating_a_request(self.path): self.wfile.write(bytes("pong\n", "utf-8"))
         else: self.wfile.write(bytes('Please, send me a "ping", I will answer you "pong"\n', "utf-8"))
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--server_address', default=False, 
-                        help='server_address')
-    parser.add_argument('-p', '--server_port', default=False, 
-                        help='server_port')
+    parser.add_argument('-a', '--server_address', default=False, help='server_address')
+    parser.add_argument('-p', '--server_port', default=False, help='server_port')
     script_arg = parser.parse_args()
     return script_arg
 
 def validating_a_request(path):
-    if path == "/ping":
-        return True
-    else: 
-        return False
+    if path == "/ping": return True
+    else: return False
 
 def main():
     try:
